f2f0d42245eb5eb97ad4f1349514841818f01a37
djlee <ddongjunn@gmail.com>
2022-12-07 23:28:58 +0900

    PRG Post/Redirect/Get

--- spring-mvc1-item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring-mvc1-item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -7,14 +7,14 @@ public class Item {
     private Long id;
     private String itemName;
     private Integer price;
-    private Integer quantitiy;
+    private Integer quantity;
 
     public Item() {
     }
 
-    public Item(String itemName, Integer price, Integer quantitiy) {
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
-        this.quantitiy = quantitiy;
+        this.quantity = quantity;
     }
 }
--- spring-mvc1-item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ spring-mvc1-item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -31,7 +31,7 @@ public class ItemRepository {
         Item findItem = findById(itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
-        findItem.setQuantitiy(updateParam.getQuantitiy());
+        findItem.setQuantity(updateParam.getQuantity());
     }
 
     public void clearStore(){
