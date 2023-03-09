66b71437d30d00c4091eb129d4b952155c0366b5
djlee <ddongjunn@gmail.com>
2023-03-10 00:10:23 +0900

    파일 다운로드

--- inflearn-spring-mvc2/upload/src/main/java/hello/upload/controller/ItemController.java
+++ inflearn-spring-mvc2/upload/src/main/java/hello/upload/controller/ItemController.java
@@ -8,15 +8,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -71,7 +74,12 @@ public class ItemController {
 
         log.info("uploadFileName={}", uploadFileName);
 
-        return ResponseEntity.ok().body(resource);
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
     }
 
 }
