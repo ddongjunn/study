706ddb7dea0521df65dace56596b8f675b9f30cd
ddongjunn <ddongjunn@gmail.com>
2024-02-16 15:11:56 +0900

    feat : 음식 주문하기

---
+++ oop-pratice-4/src/main/java/org/example/Cook.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Cook {
+    private final String name;
+    private final int price;
+
+    public Cook(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Cook(MenuItem menuItem){
+        this.name = menuItem.getName();
+        this.price = menuItem.getPrice();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cook cook = (Cook) o;
+
+        if (price != cook.price) return false;
+        return Objects.equals(name, cook.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + price;
+        return result;
+    }
+}
---
+++ oop-pratice-4/src/main/java/org/example/MenuItem.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Objects;
+
+public class MenuItem {
+    private final String name;
+    private final int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean matches(String name) {
+        return this.name.equals(name);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuItem menuItem = (MenuItem) o;
+
+        if (price != menuItem.price) return false;
+        return Objects.equals(name, menuItem.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + price;
+        return result;
+    }
+}
