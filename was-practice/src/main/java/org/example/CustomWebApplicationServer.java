9dc226018b98698a146b57a5e9006b512ecb38da
ddongjunn <ddongjunn@gmail.com>
2024-02-16 17:51:56 +0900

    feat : 계산기 웹 프로그램

---
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class CustomWebApplicationServer {
+
+    private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
+
+    public CustomWebApplicationServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException{
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            logger.info("[CustomWebApplicationServer] started {} port", port);
+
+            Socket clientSocket;
+            logger.info("[CustomWebApplicationServer] waiting for client");
+
+            while((clientSocket = serverSocket.accept()) != null){
+                logger.info("[CustomWebApplicationServer] client connected!");
+
+                /**
+                 * Step3 - Thread Pool을 적용해서 안정적인 서비스로 구현
+                 */
+                executorService.execute(new ClientRequestHandler(clientSocket));
+
+                /**
+                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                 */
+                // new Thread(new ClientRequestHandler(clientSocket)).start();
+
+                /**
+                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 */
+                /*try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    HttpRequest httpRequest = new HttpRequest(br);
+
+                    // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
+
+                        QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                        String operator = queryStrings.getValue("operator");
+                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                        byte[] body = String.valueOf(result).getBytes();
+
+                        HttpResponse httpResponse = new HttpResponse(dos);
+                        httpResponse.response200Header("application/json", body.length);
+                        httpResponse.responseBody(body);
+                    }
+                }*/
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
